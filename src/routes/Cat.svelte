d6399149e8f33b28ebf5c8172783220f031d1715
Alexander Starikov <acherryjam@gmail.com>
2024-08-16 20:16:05 +0300

    feat: add sounds after feeding the cat

--- src/routes/Cat.svelte
+++ src/routes/Cat.svelte
@@ -1,11 +1,45 @@
 <script lang="ts">
-    import { candiesEaten } from "./store";
+	import { onMount } from "svelte";
     import { droppable } from "$lib/dnd";
+    import { getRandomInt } from "$lib/utils";
+    import { candiesEaten } from "./store";
+
+    let audio: any;
+    onMount(() => {
+        audio;
+    });
 
     function dropelement(e: CustomEvent) {
         let elem: HTMLElement = e.detail;
         elem.remove();
         $candiesEaten++;
+
+         // 5% chance to play a sound
+        if (Math.random() < 0.05) {
+            const r = Math.random();
+
+            let src = "/audio/";
+            if (r < 0.5) {
+                // 50% chance to purr
+                src += `purr${getRandomInt(1, 4)}.mp3`;
+            } else if (r < 0.9) {
+                // 40% chance to meow
+                src += `meow${getRandomInt(1, 3)}.mp3`
+            } else {
+                // 10% chance for a looong purr
+                src += "purrUnique1.mp3";
+            }
+
+            playAudio(src);
+        }
+    }
+
+    function playAudio(src: string, volume: number = 0.05) {
+        if (audio && audio.duration > 0 && !audio.paused) audio.pause();
+
+        audio = new Audio(src);
+        audio.volume = volume;
+        audio.play();
     }
 </script>
 
